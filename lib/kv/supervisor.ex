4c5474645e92f8bdd5315491da11bb475c8086da
Iggy <igoririanto@rocketmail.com>
2018-03-23 12:42:28 -0500

    edit registry and supervisor and pass all tests

--- lib/kv/supervisor.ex
+++ lib/kv/supervisor.ex
@@ -12,16 +12,4 @@ defmodule KV.Supervisor do
     ]
     Supervisor.init(children, strategy: :one_for_all)
   end
-
-  def handle_cast({:create, name}, {names, refs}) do
-    if Map.has_key?(names, name) do
-      {:noreply, {names, refs}}
-    else
-      {:ok, pid} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
-      ref = Process.monitor(pid)
-      refs = Map.put(refs, ref, name)
-      names = Map.put(names, name, pid)
-      {:noreply, {names, refs}}
-    end
-  end
 end
