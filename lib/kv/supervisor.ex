e748d1a8ca94b92058b97161264690e0c891a476
Iggy <igoririanto@rocketmail.com>
2018-03-23 12:28:39 -0500

    add DynamicSupervisor

--- lib/kv/supervisor.ex
+++ lib/kv/supervisor.ex
@@ -7,8 +7,21 @@ defmodule KV.Supervisor do
 
   def init(:ok) do
     children = [
-      {KV.Registry, name: KV.Registry}
+      {KV.Registry, name: KV.Registry},
+      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one}
     ]
     Supervisor.init(children, strategy: :one_for_one)
+  end
+
+  def handle_cast({:create, name}, {names, refs}) do
+    if Map.has_key?(names, name) do
+      {:noreply, {names, refs}}
+    else
+      {:ok, pid} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
+      ref = Process.monitor(pid)
+      refs = Map.put(refs, ref, name)
+      names = Map.put(names, name, pid)
+      {:noreply, {names, refs}}
+    end
   end
 end
